4fe7226c3fda56894be9bad5e98a10660251bd8d
CarlaCangussu <karlacangussu@hotmail.com>
2019-06-11 00:41:29 -0300

    add classe viagem

---
+++ src/viagem.java
@@ -0,0 +1,25 @@
+
+public class viagem {
+	protected double distancia;
+	protected double tempo;
+	protected int carga;
+	public double getDistancia() {
+		return distancia;
+	}
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	public double getTempo() {
+		return tempo;
+	}
+	public void setTempo(double tempo) {
+		this.tempo = tempo;
+	}
+	public int getCarga() {
+		return carga;
+	}
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+
+}
